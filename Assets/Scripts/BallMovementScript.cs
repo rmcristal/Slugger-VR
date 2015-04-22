248fb6472a5571a6e83ab4db44a2b9829e538d0e
Ryan <ryan.m.cristal@gmail.com>
2015-04-21 21:14:51 -0600

    Initial Commit

---
+++ Assets/Scripts/BallMovementScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovementScript : MonoBehaviour {
+
+	public Rigidbody ballPrefab;
+	private Rigidbody ballClone;
+	public int pitchSpeed = 10;
+	private GameObject other;
+	private bool started = false;
+	
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	if (started == false && Input.GetKeyDown(KeyCode.S)) 
+		{
+			StartCoroutine ("Pitch");
+			started = true;
+		}
+	}
+
+
+
+
+	IEnumerator Pitch()
+	{
+		while (true)
+		{
+			ballClone = Instantiate (ballPrefab) as Rigidbody;
+			ballClone.AddForce (transform.forward * -pitchSpeed);
+			yield return new WaitForSeconds(Random.Range (3.5f,4.2f));
+			Destroy(ballClone.gameObject);
+
+		}
+	}
+
+}
